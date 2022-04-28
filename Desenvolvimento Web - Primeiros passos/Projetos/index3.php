e715e9310700ddc4864292cfa8e81c2c07bf9904
vbonds <vanessabonds@hotmail.com>
2022-04-28 15:51:45 -0300

    Inclusão de exercicios de PHP e desenvolvimento web

---
+++ Desenvolvimento Web - Primeiros passos/Projetos/index3.php
@@ -0,0 +1,52 @@
+<html lang="br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>jQuery UI Accordion - Default functionality</title>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="/resources/demos/style.css">
+		<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+		<script>
+			$( function() {
+			$( "#accordion" ).accordion();
+			} );
+		</script>
+	</head>
+	
+	<body>
+		<h1>Projeto site DIO</h1></br>
+		<div id="accordion">
+		
+		<h3>Projeto site DIO</h3>
+		<div>
+			<p>
+				Criação de um site com a funcionalidade jquery accordion para teste de aprendizado da matéria
+			</p>
+		</div>
+		<h3>HTML</h3>
+			<div>
+				<p>
+					A marcação HTML subjacente é uma série de cabeçalhos (tags H3) e divs de conteúdo para que o conteúdo seja utilizável sem JavaScript.
+				</p>
+			</div>
+		<h3>Jquery Accordion</h3>
+		<div>
+			<p>
+				<img src="R.jpg"></img>
+			</p>
+			<ul>
+				<li>List Jquery accordion</li>
+				<li>List Site PHP</li>
+				<li>List Aprendendo na pratica</li>
+			</ul>
+		</div>
+		<h3>Projeto site Finalizado</h3>
+			<div>
+				<p>
+					Finalizado o projeto de site criado	!!
+				</p>
+			</div>
+	</div>
+	</body>
+</html>
